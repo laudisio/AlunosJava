f76857a89b3a54ad5c4c15a2c071246f46cc2f66
Lucas239 <54159695+Lucas239@users.noreply.github.com>
2020-04-13 16:39:43 -0300

    Versão 5.1

--- Aluno.java
+++ Aluno.java
@@ -1,25 +1,25 @@
 public class Aluno {
 
-	private String nome;
-	private int id;
-	
-	public Aluno(String nome, int id) {
-		super();
-		nome = nome;
-		id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	
+    private String nome;
+    private int id;
+
+    public Aluno(String nome, int id) {
+        super();
+        this.nome = nome;
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
 }
