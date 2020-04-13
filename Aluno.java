ec7191eb690862a61634d19528de44bc6096a382
laudisio <61565218+laudisio@users.noreply.github.com>
2020-04-13 16:37:36 -0300

    Versão 5.0

--- Aluno.java
+++ Aluno.java
@@ -3,6 +3,11 @@ public class Aluno {
 	private String nome;
 	private int id;
 	
+	public Aluno(String nome, int id) {
+		super();
+		nome = nome;
+		id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
